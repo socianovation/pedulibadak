478ffad36923723f8606a0d06ddb6f59d6cafb5d
summon23 <andreymahdison@gmail.com>
2017-09-28 11:28:58 +0700

    .

--- application/views/home/gallery_view.php
+++ application/views/home/gallery_view.php
@@ -1,57 +1,58 @@
+<?php $this->load->view('templates/header_view'); ?>
 <div class="grey-section pt20 list-page-container">
-	<div class="container container-p20 list-page-title tac">
-        <div class="blog-title fwb">
-            GALLERY
-        </div>
+<div class="container container-p20 list-page-title tac">
+    <div class="blog-title fwb">
+        GALLERY
     </div>
-	<div class="container container-p40">
-		<div class="row card-container">
-                <div class="col-sm-6 col-md-4 card-item-col">
-                    <div class="card-item">
-                        <div class="card-item-img" style="background-image: url('images/rhino.jpg')"></div>
-                        <div class="clearfix card-border">
-                            <a href="detail.html" class="card-item-link fwb ttu p20 dib">
-                                Lihat gambar
-                                <i class="fa fa-long-arrow-right ml5"></i>
-                            </a>
-                        </div>
+</div>
+<div class="container container-p40">
+    <div class="row card-container">
+            <div class="col-sm-6 col-md-4 card-item-col">
+                <div class="card-item">
+                    <div class="card-item-img" style="background-image: url('images/rhino.jpg')"></div>
+                    <div class="clearfix card-border">
+                        <a href="detail.html" class="card-item-link fwb ttu p20 dib">
+                            Lihat gambar
+                            <i class="fa fa-long-arrow-right ml5"></i>
+                        </a>
                     </div>
                 </div>
-                <div class="col-sm-6 col-md-4 card-item-col">
-                    <div class="card-item">
-                        <div class="card-item-img" style="background-image: url('images/img-tips.png')"></div>
-                        <div class="clearfix card-border">
-                            <a href="detail.html" class="card-item-link fwb ttu p20 dib">
-                                Lihat gambar
-                                <i class="fa fa-long-arrow-right ml5"></i>
-                            </a>
-                        </div>
+            </div>
+            <div class="col-sm-6 col-md-4 card-item-col">
+                <div class="card-item">
+                    <div class="card-item-img" style="background-image: url('images/img-tips.png')"></div>
+                    <div class="clearfix card-border">
+                        <a href="detail.html" class="card-item-link fwb ttu p20 dib">
+                            Lihat gambar
+                            <i class="fa fa-long-arrow-right ml5"></i>
+                        </a>
                     </div>
                 </div>
-		</div>
-		<div class="row tac mt20">
-			<a href="page.html" class="btn btn-arrow-red fwb ttu">
-				Gambar lainnya
-				<i class="fa fa-long-arrow-right"></i>
-			</a>
-		</div>
-	</div>
+            </div>
+    </div>
+    <div class="row tac mt20">
+        <a href="page.html" class="btn btn-arrow-red fwb ttu">
+            Gambar lainnya
+            <i class="fa fa-long-arrow-right"></i>
+        </a>
+    </div>
+</div>
 </div>
 <div class="blog-section" style="background-image: url('images/img-tips.png')">
-	<div class="container">
-		<div class="row tac">
-			<div class="blog-title fwb">
-				BADAK
-			</div>
-			<div class="fwb fz20">
-					Save the Rhino International’s vision is for all five rhino species to thrive in the wild for future generations.
-			</div>
-		</div>
-		<div class="row tac mt40">
-			<a href="page.html" class="btn btn-arrow-white fwb ttu">
-				Read tips
-				<i class="fa fa-long-arrow-right"></i>
-			</a>
-		</div>
-	</div>
+<div class="container">
+    <div class="row tac">
+        <div class="blog-title fwb">
+            BADAK
+        </div>
+        <div class="fwb fz20">
+                Save the Rhino International’s vision is for all five rhino species to thrive in the wild for future generations.
+        </div>
+    </div>
+    <div class="row tac mt40">
+        <a href="page.html" class="btn btn-arrow-white fwb ttu">
+            Read tips
+            <i class="fa fa-long-arrow-right"></i>
+        </a>
+    </div>
+</div>
 </div>
